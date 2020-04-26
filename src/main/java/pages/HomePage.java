d5d545cdd81175fc69d5e15108dcbd72863a2e44
Shukhrat <shuhratfaiziev@gmail.com>
2020-04-26 17:58:47 -0400

    first commit

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is Home Page Objects");
+		
+	}
+
+}
